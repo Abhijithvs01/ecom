797deca6c9d14c302f71acd99f22cc94e7014d7a
Abhijith  V S <abhijithvs0102@gmail.com>
2026-05-15 14:11:12 +0530

    4

--- products/views.py
+++ products/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+def list_product(request):
+    return render(request, 'shop.html')
+def deatil_product(request):
+    return render(request, 'shop-single.html' )
+
