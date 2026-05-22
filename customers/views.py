30abe99232e0c442ad151cd179eaa648d4302a93
Abhijith  V S <abhijithvs0102@gmail.com>
2026-05-23 00:05:32 +0530

    commit

--- customers/views.py
+++ customers/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 
 def login_page(request):
     return render(request, 'login.html')
+def register(request):
+    return render(request,'register.html')
--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from . models import products
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 def list_product(request):
+
+    product_list = products.objects.all()
+    context = {'products':product_list}
     return render(request, 'shop.html')
 def deatil_product(request):
     return render(request, 'shop-single.html' )
