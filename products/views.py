7571a82490bed615a810b3f13e93dc4ce60194ed
Abhijith  V S <abhijithvs0102@gmail.com>
2026-05-26 22:37:14 +0530

    6

--- products/views.py
+++ products/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from . models import products
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 def list_product(request):
-
+    page = 1
+    if request.GET:
+        page = request.GET.get('page',1)
     product_list = products.objects.all()
+    product_paginator = Paginator(product_list,1)
+    product_list = product_paginator.get_page(page)
     context = {'products':product_list}
-    return render(request, 'shop.html')
+    return render(request, 'shop.html', context)
 def deatil_product(request):
     return render(request, 'shop-single.html' )
 
