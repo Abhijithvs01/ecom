2723c0a0e5c5755dbd9b9e38ee7faf8184731018
Abhijith  V S <abhijithvs0102@gmail.com>
2026-06-02 02:54:06 +0530

    8

--- customers/views.py
+++ customers/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from . models import customer
 
 # Create your views here.
 
 def login_page(request):
+    if request.POST and 'login' in request.POST:
+        email = request.POST.get('email') 
+        password = request.POST.get('password')
     return render(request, 'login.html')
 def register(request):
+    if request.POST and 'register' in request.POST:
+        try:
+            username = request.POST.get('fullname')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            confirm_password = request.POST.get('confirm_password')
+            address = request.POST.get('address')
+            phonenumber = request.POST.get('phonenumber')
+
+            #create an user account
+            user = User.objects.create(
+                username = username,
+                email = email,
+                password = password
+            )
+
+            customers = customer.objects.create(
+                user = user,
+                phone = phonenumber,
+                address = address
+            )
+            return redirect('/')
+        except Exception as e:
+            error_message = "Duplicate username or invalid credentials"
+            messages.error(request,error_message,'error_message')
+
     return render(request,'register.html')
